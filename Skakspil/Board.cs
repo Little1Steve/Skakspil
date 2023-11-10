7af054e9275221035b4f930f68e72543aeb1afe3
Zh4d3 <lugiraun@gmail.com>
2023-11-10 11:05:43 +0100

    display

--- Skakspil/Board.cs
+++ Skakspil/Board.cs
@@ -8,17 +8,17 @@ namespace Skakspil
 {
     internal class Board
     {
-        /*Pieces[,] board;
-        King blackKing = new King();
-        King whiteKing = new King();
+        Pieces[,] board;
+        King blackKing = new King()[4][0];
+        King whiteKing = new King()[4][7];
 
-        Queen blackQueen = new Queen();
-        Queen whiteQueen = new Queen();
+        Queen blackQueen = new Queen()[3][0];
+        Queen whiteQueen = new Queen()[3][7];
 
-        Bishop blackBishop1 = new Bishop();
-        Bishop blackBishop2 = new Bishop();
-        Bishop whiteBishop1 = new Bishop();
-        Bishop whiteBishop2 = new Bishop();
+        Bishop blackBishop1 = new Bishop()[2][0];
+        Bishop blackBishop2 = new Bishop()[5][0];
+        Bishop whiteBishop1 = new Bishop()[2][7];
+        Bishop whiteBishop2 = new Bishop()[5][7];
 
         Knight blackKnight1 = new Knight();
         Knight blackKnight2 = new Knight();
@@ -28,7 +28,7 @@ namespace Skakspil
         Rook blackRook1 = new Rook();
         Rook blackRook2 = new Rook();
         Rook whiteRook1 = new Rook();
-        Rook whiteRook2 = new Rook();*/
+        Rook whiteRook2 = new Rook();
 
     }
 }
