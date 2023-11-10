3a13abead1743bf99b69e1b4cf40fccc109d7bc1
Little1Steve <estraarupnielsen@gmail.com>
2023-11-10 11:04:45 +0100

    dwdasds

--- Skakspil/Board.cs
+++ Skakspil/Board.cs
@@ -8,27 +8,7 @@ namespace Skakspil
 {
     internal class Board
     {
-        /*Pieces[,] board;
-        King blackKing = new King();
-        King whiteKing = new King();
-
-        Queen blackQueen = new Queen();
-        Queen whiteQueen = new Queen();
-
-        Bishop blackBishop1 = new Bishop();
-        Bishop blackBishop2 = new Bishop();
-        Bishop whiteBishop1 = new Bishop();
-        Bishop whiteBishop2 = new Bishop();
-
-        Knight blackKnight1 = new Knight();
-        Knight blackKnight2 = new Knight();
-        Knight whiteKnight1 = new Knight();
-        Knight whiteKnight2 = new Knight();
-
-        Rook blackRook1 = new Rook();
-        Rook blackRook2 = new Rook();
-        Rook whiteRook1 = new Rook();
-        Rook whiteRook2 = new Rook();*/
+        public Pieces[,]? board;
 
     }
 }
